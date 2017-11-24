a61b93ced57e1e603e84f6bf2fba872b9c2b2d6d
Aitor Miguel Blanco <aitormibl@gmail.com>
2017-11-24 13:58:22 +0100

    restructure of code folder and inclusion of java scripts

---
+++ getweather.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import pyowm
+
+owm = pyowm.OWM('da5438549b419b84ea6890de543fb25c')  # a valid API key
+
+# Have a pro subscription? Then use:
+# owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
+
+# Will it be sunny tomorrow at this time in Milan (Italy) ?
+forecast = owm.daily_forecast("Odense,dk")
+tomorrow = pyowm.timeutils.tomorrow()
+forecast.will_be_sunny_at(tomorrow)  # Always True in Italy, right? ;-)
+
+# Search for current weather in London (UK)
+observation = owm.weather_at_place('Odense,dk')
+w = observation.get_weather()
+print(w.get_temperature('celsius'))                      # <Weather - reference time=2013-12-18 09:20,
+                              # status=Clouds>
+
+# Weather details
+w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+w.get_humidity()              # 87
+w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+
+# Search current weather observations in the surroundings of
+# lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
+observation_list = owm.weather_around_coords(-22.57, -43.12)
